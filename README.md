6988bcb81be7e45e62858a5d4fc08e8205eacc5c
zhangxiaolong88 <xlzhang1988@gmail.com>
2014-09-28 16:16:33 +0800

    init

---
+++ README.md
@@ -0,0 +1,27 @@
+angular-require-grunt-express-seed
+==================================
+
+能够自动化部署的种子项目（包括前后端）
+
+##依赖：
+###1.nodejs
+http://www.nodejs.org/
+
+###2.Ruby
+gem install sass<br />
+gem install compass
+
+###3.grunt
+npm install -g grunt-cli
+
+###4.bower
+npm install –g bower
+
+##安装了上述服务后进行初始化：
+bower install & npm install
+
+##启动服务：
+grunt express-server
+
+##打开浏览器：
+http://localhost:9000
---
+++ app/scripts/routers/routers.js
@@ -0,0 +1,36 @@
+﻿'use strict';
+
+define([], function() {
+	return {
+		home: {
+			title: '首页',
+			route: '/',
+			template: 'views/main.html',
+			controller: 'appController'
+		},
+		worldmap: {
+			title: '一级目录',
+			list: {
+				active: {
+					title: '在线人数',
+					route: '/worldmap-active',
+					template: 'views/worldMapActive.html',
+					controller: 'worldMapActiveController'
+				},
+				money: {
+					title: '充值金额',
+					route: '/worldmap-money',
+					template: 'views/worldMapMoney.html',
+					controller: 'worldMapMoneyController'
+				},
+				register: {
+					title: '注册人数',
+					route: '/worldmap-register',
+					template: 'views/worldMapRegister.html',
+					controller: 'worldMapRegisterController'
+				}
+			}
+		}
+
+	};
+})
